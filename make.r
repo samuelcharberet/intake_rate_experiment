0bbe6bbe4f5d50c674245a1ec9e2860784f0614f
Samuel Charberet <samuelcharberet@gmail.com>
2021-12-14 17:39:55 +0100

    updated the make.R

--- make.r
+++ make.r
@@ -3,3 +3,5 @@
 lapply(list.files(here::here("R"), recursive = TRUE, full.names = T), source)
 
 data_irn_individuals <- load_individual_data(path=here::here("1_data", "data_irn_individuals.csv"))
+
+data_irn_food_control <- load_food_control_data(path=here::here("1_data", "data_irn_food_controls.csv"))
